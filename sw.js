d30bfdaaea3ef3408ddcf7b392f35dac6c9972bf
shahazadatalibkhan <72077549+shahazadatalibkhan@users.noreply.github.com>
2026-08-03 23:01:51 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'ms-agency-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS).catch(() => {})));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
